61bb7e0f9c9479abf2d3282b793c464fbf94b954
Leonardo Martins <lolcomeisback@gmail.com>
2025-06-14 17:17:58 -0300

    :sparkle: rato andando e pulando

--- Assets/Scripts/Moviment.cs
+++ Assets/Scripts/Moviment.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Moviment : MonoBehaviour
 {
     private CharacterController controller;
+    private Transform camera;
+    private Animator animator;
+    private bool estaNoChao = true;
+    [SerializeField] private Transform peDoPersonagem;
+    [SerializeField] private LayerMask colisaoLayer;
+    private float forcaY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        if (controller != null)
-        {
-            print("CharacterController found and initialized.");
-        }
+        camera = Camera.main.transform;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +24,30 @@ public class Moviment : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movimento = new Vector3(horizontal, -9.8f, vertical);
+        Vector3 movimento = new Vector3(horizontal, 0, vertical);
+
+        movimento = camera.TransformDirection(movimento);
+        movimento.y = 0;
+
         controller.Move(movimento * Time.deltaTime * 5f);
+        if (movimento != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movimento), Time.deltaTime * 10f);
+        }
+
+        animator.SetBool("Mover", movimento != Vector3.zero);
+
+        estaNoChao = Physics.CheckSphere(peDoPersonagem.position, 0.3f, colisaoLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao)
+        {
+            forcaY = 10;
+        }
+        if(forcaY > -9.81f && !estaNoChao)
+        {
+            forcaY -= 9.81f * Time.deltaTime;
+        }
+        
+        controller.Move(new Vector3(0, forcaY, 0) * Time.deltaTime);
     }
 }
